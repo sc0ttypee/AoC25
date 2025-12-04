bbc6d275d01fc1cbe9f659b0ab6cd695cef11f06
Aaron Robert Scott <aaron.scott@student.kit.edu>
2025-12-04 01:45:36 +0100

    day_03_final

--- 3.py
+++ 3.py
@@ -1,37 +1,11 @@
 import sys
-import time
-import math
 
 if __name__ == "__main__":
-    start_time = time.time()
 
     print("Go!", flush=True)
     input_data = sys.stdin.read().splitlines()
-    
     result_1, result_2 = 0, 0
 
-    for line in input_data:
-        int_list = [int(x) for x in line]
-        int_list.reverse()
-        lowest = 1
-        while (int_list.__len__() > 12):
-            try:
-                int_list.pop(int_list.index(lowest))
-            except:
-                lowest +=1
-                continue
-
-        int_list.reverse()
-        counter = 11
-        res = 0
-        for x in int_list:
-            res += x * 10 ** counter
-            counter -= 1
-        print(res)
-        result_2 += res
-    print(result_2)
-
-def part_1(input_data):
     for line in input_data:
         int_list = [int(x) for x in line]
         last = int_list[-1]
@@ -43,7 +17,6 @@ def part_1(input_data):
             try:
                 idx = rest.index(max_val)
                 highest_left = rest[idx]
-                print(f"highest left: {highest_left}")
                 rest = rest[idx + 1:]
             except:
                 max_val -= 1
@@ -53,16 +26,28 @@ def part_1(input_data):
             try:
                 idx = rest.index(max_val)
                 highest_right = rest[idx]
-                print(f"highest right: {highest_right}")
             except:
                 max_val -= 1
                 continue
         if (highest_right < last):
             result_1 += highest_left * 10 + last
-            print(highest_left * 10 + last)
         else:
             result_1 += highest_left * 10 + highest_right
-            print(highest_left * 10 + highest_right)
 
+    zip_with = [10 ** x for x in range(11, -1, -1)]
 
-    print(f"Part 1: {result_1}\nPart 2: {result_2}\nSolved in {round((time.time() - start_time) * 1000, 2)} ms", flush=True)
+    for line in input_data:
+        int_list = [int(x) for x in line]
+        delete_budget = int_list.__len__() - 12
+        result = []
+        for digit in int_list:
+            while result.__len__() > 0 and result[-1] < digit and delete_budget > 0:
+                result.pop()
+                delete_budget -= 1
+            result.append(digit)
+        result = result[:12]
+        line_value = 0
+        for idx, elem in enumerate(result):
+            line_value += elem * zip_with[idx]
+        result_2 += line_value
+    print(f"Part 1: {result_1}\nPart 2: {result_2}", flush=True)
