d58e1b9ae2d7200c32e89528581313b7e53f8d3e
Aaron Robert Scott <aaron.scott@student.kit.edu>
2025-12-05 12:41:14 +0100

    day_04 optimized

--- 4.py
+++ 4.py
@@ -2,70 +2,55 @@ import sys
 import time
 
 if __name__ == "__main__":
-    start_time = time.time()
-
     print("Go!", flush=True)
-    input_data = None
-    first_run = True
-
     input_data = sys.stdin.read().strip()
+
     # with open("test.txt", "r", encoding="utf-8") as file:
-    #     input_data = file.read() 
+    #     input_data = file.read().strip()
 
     result_1, result_2 = 0, 0
-    directions = [(x, y) for x in range(-1, 2) for y in range(-1, 2) if not (x == 0 and y == 0)]
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (-1, -1), (1, -1), (-1, 1), (1, 1)]
 
     input_list = input_data.split("\n")
 
-    input = [list(elem) for elem in input_list]
+    row_len = len(input_list)
+    col_len = len(input_list[0])
 
-    for i in range(0, input.__len__()):
-        for j in range(0, input[0].__len__()):
-            adj_value = 0
+    input_list = ['.' * row_len] + input_list + ['.' * row_len]
+    input = [list('.' + elem + '.') for elem in input_list]
+    marked = set()
+    for i in range(1, row_len + 1):
+        for j in range(1, col_len + 1):
+            adj_value = 0   
             for direction in directions:
-                i_offset = i + direction[0]    
-                j_offset = j + direction[1]
-                if (i_offset < 0 or i_offset > input.__len__() - 1 or j_offset < 0 or j_offset > input[0].__len__() - 1):
-                    continue
-                else:
-                    if (input[i_offset][j_offset] == '@'):
-                        adj_value += 1
-                    else:
-                        continue
-            if (adj_value < 4 and input[i][j] == '@'):
-                result_1 += 1
-    run_result = -1
-    indices = []
-    while (run_result != 0):
-        for i in range(0, input.__len__()):
-            for j in range(0, input[0].__len__()):
-                adj_value = 0
-                for direction in directions:
-                    i_offset = i + direction[0]    
-                    j_offset = j + direction[1]
-                    if (i_offset < 0 or i_offset > input.__len__() - 1 or j_offset < 0 or j_offset > input[0].__len__() - 1):
-                        continue
-                    else:
-                        if (input[i_offset][j_offset] == '@'):
-                            adj_value += 1
-                        else:
-                            continue
-                if (adj_value < 4 and input[i][j] == '@'):
-                    if (run_result == -1):
-                        run_result += 2
-                    else:
-                        run_result += 1
-                    indices.append((i, j))
-        if (run_result == -1):
-            break
-        result_2 += run_result
-        for index in indices:
-            input[index[0]].pop(index[1])
-            input[index[0]].insert(index[1], '.')
-        if (run_result != 0):
-            run_result = -1
-
-
+                if (input[i + direction[0]][j + direction[1]] == '@'):
+                    adj_value += 1
+            if (adj_value) < 4:
+                marked.add((i, j))
+    for mark in marked:
+        if (input[mark[0]][mark[1]] == '@'):
+            result_1 += 1
+            input[mark[0]][mark[1]] = '.'
     
+    result_2 = result_1
+    marked.clear()
 
-    print(f"Part 1: {result_1}\nPart 2: {result_2}\nSolved in {round((time.time() - start_time) * 1000, 2)} ms", flush=True)
+    while True:
+
+        for i in range(1, row_len + 1):
+            for j in range(1, col_len + 1):
+                adj_value = 0   
+                for direction in directions:
+                    if (input[i + direction[0]][j + direction[1]] == '@'):
+                        adj_value += 1
+                if (adj_value) < 4 and input[i][j] == '@':
+                    marked.add((i, j))
+        if marked.__len__() == 0:
+            break
+        for mark in marked:
+            if (input[mark[0]][mark[1]] == '@'):
+                result_2 += 1
+                input[mark[0]][mark[1]] = '.'
+        marked.clear()
+
+    print(f"Part 1: {result_1}\nPart 2: {result_2}\n", flush=True)
